a1b7a5057b26d37691f6edd6bdb87fdf0968c027
Or Dahari <ordahari@outlook.co.il>
2018-03-24 00:48:37 +0300

    ADD Singleton

---
+++ Singelton/ChocolateBoler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class ChocolateBoler
+    {
+        private static volatile ChocolateBoler instance;
+        private static object syncRoot = new Object();
+        private bool empty;
+        private bool boiled;
+        private ChocolateBoler()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        public static ChocolateBoler Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new ChocolateBoler();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public void Fill()
+        {
+            if (empty)
+            {
+                empty = false;
+                boiled = true;
+                Console.WriteLine("fill the boiler with a milk / chocolate ");
+            }
+        }
+
+        public void Drain()
+        {
+            if (!empty && boiled)
+            {
+                empty = true;
+                boiled = false;
+                Console.WriteLine("drain the boiled milk and chocolate");
+            }
+        }
+
+        public void Boiled()
+        {
+            if (!empty && !boiled)
+            {
+                boiled = true;
+                Console.WriteLine("bring the content to a boil");
+            }
+
+        }
+
+        public bool IsBoiled()
+        {
+            return boiled;
+        }
+
+        public bool IsEmpty()
+        {
+            return empty;
+        }
+    }
+}
---
+++ Singelton/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Threading;
+
+namespace Singleton
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            ChocolateBoler chocolateBoler = ChocolateBoler.Instance;
+
+            Thread thread1 = new Thread(SomeThread);
+            thread1.Start();
+            chocolateBoler.Fill();
+            Console.WriteLine("IsEmpty:" + chocolateBoler.IsEmpty());
+            Console.WriteLine("IsBoiled:" + chocolateBoler.IsBoiled());
+            chocolateBoler.Boiled();
+            Console.WriteLine("IsEmpty:" + chocolateBoler.IsEmpty());
+            Console.WriteLine("IsBoiled:" + chocolateBoler.IsBoiled());
+            chocolateBoler.Drain();
+            Console.WriteLine("IsEmpty:" + chocolateBoler.IsEmpty());
+            Console.WriteLine("IsBoiled:" + chocolateBoler.IsBoiled());
+
+            Console.Read();
+        }
+
+        public static void SomeThread(object obj)
+        {
+            var _chocolateBoler = ChocolateBoler.Instance;
+            while (_chocolateBoler.IsBoiled())
+            {
+
+
+            }
+            Console.WriteLine("END SomeThread");
+        }
+    }
+}
